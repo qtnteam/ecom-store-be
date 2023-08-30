512b6ad00588363db6bafde3a5be47b83c0602c6
vo.trong.thanh <vo.trong.thanh@sun-asterisk.com>
2023-08-30 12:14:59 +0900

    chore: config ut and update ci

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,6 @@
+// gkc_hash_code : 01GYS4MFBRHRYQ4ENZEFBHPDA0
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
