dd73b1f57d6c589c5571263d19a7a05569d0cddc
thanhvt-3018 <vo.trong.thanh@sun-asterisk.com>
2023-09-05 23:31:50 +0700

    feat(#32): batch create category

---
+++ src/modules/category/entities/category.entity.ts
@@ -0,0 +1,59 @@
+// gkc_hash_code : 01GYS4MFBRHRYQ4ENZEFBHPDA0
+import { Column, Entity, JoinColumn, ManyToOne, OneToMany } from 'typeorm';
+
+import { EntityConstant } from '@/constants/entity.constant';
+import { CategoryLevelEnum } from '@/constants/enum';
+import { AbstractEntity, IAbstractEntity } from '@/shared/common/base.entity';
+import { UseDto } from '@/shared/decorators/use-dto.decorator';
+
+import { CategoryDto } from '../dto/category.dto';
+
+@Entity('categories')
+@UseDto(CategoryDto)
+export class Category
+  extends AbstractEntity<CategoryDto>
+  implements IAbstractEntity<CategoryDto>
+{
+  @ManyToOne(() => Category, (category) => category.childCategories)
+  @JoinColumn({ name: 'parent_id' })
+  parentCategory: Category;
+
+  @OneToMany(() => Category, (category) => category.parentCategory)
+  childCategories: Category[];
+
+  @Column({
+    name: 'name',
+    type: 'varchar',
+    length: EntityConstant.EntityShortLength,
+  })
+  name: string;
+
+  @Column({
+    name: 'display_name',
+    type: 'varchar',
+    length: EntityConstant.EntityShortLength,
+  })
+  displayName: string;
+
+  @Column({
+    name: 'parent_id',
+    type: 'varchar',
+    nullable: true,
+  })
+  parentId: string;
+
+  @Column({
+    name: 'level',
+    type: 'tinyint',
+    default: CategoryLevelEnum.Level1,
+    unsigned: true,
+  })
+  level: CategoryLevelEnum;
+
+  @Column({
+    name: 'slug',
+    type: 'varchar',
+    length: EntityConstant.EntityShortLength,
+  })
+  slug: string;
+}
